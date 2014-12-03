4472580a23e17145700ac388815ac914486815f7
mrahman <mohammad.rahman@hbo.com>
2014-12-03 13:46:06 -0500

    core-exam

---
+++ src/math/problem/Pattern.java
@@ -0,0 +1,13 @@
+package math.problem;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		/* Read this numbers, find the pattern then implement the logic from this pattern.which will give you this output.
+		 * 
+		 * 1,2,3,4,5,10,12,14,16,18,23,26,29,32,35,40,44,48,52,56,61,66,71,76,81.......n
+		 * 
+		 */
+	}
+
+}
